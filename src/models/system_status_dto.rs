255f944dc9bc0205587ec74bde9e8f07332a3bf4
ericg2 <l.ittlelettere@gmail.com>
2025-02-25 12:02:17 -0500

    API Update

--- src/models/system_status_dto.rs
+++ src/models/system_status_dto.rs
@@ -13,43 +13,49 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct SystemStatusDto {
-    #[serde(rename = "cpUs", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
-    pub cp_us: Option<Option<Vec<models::CpuItemDto>>>,
-    #[serde(rename = "averageCPU", skip_serializing_if = "Option::is_none")]
-    pub average_cpu: Option<Box<models::CpuItemDto>>,
-    #[serde(rename = "apiUptime", skip_serializing_if = "Option::is_none")]
-    pub api_uptime: Option<String>,
-    #[serde(rename = "systemUptime", skip_serializing_if = "Option::is_none")]
-    pub system_uptime: Option<String>,
+    #[serde(rename = "cpus", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub cpus: Option<Option<Vec<models::CpuItemDto>>>,
+    #[serde(rename = "errors", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub errors: Option<Option<Vec<models::Error>>>,
+    #[serde(rename = "apiUptime", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub api_uptime: Option<Option<String>>,
+    #[serde(rename = "systemUptime", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub system_uptime: Option<Option<String>>,
     #[serde(rename = "apiVersion", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
     pub api_version: Option<Option<String>>,
     #[serde(rename = "kernelVersion", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
     pub kernel_version: Option<Option<String>>,
-    #[serde(rename = "totalUsersRegistered", skip_serializing_if = "Option::is_none")]
-    pub total_users_registered: Option<i64>,
-    #[serde(rename = "totalDataPoints", skip_serializing_if = "Option::is_none")]
-    pub total_data_points: Option<i64>,
-    #[serde(rename = "totalRepoPoints", skip_serializing_if = "Option::is_none")]
-    pub total_repo_points: Option<i64>,
-    #[serde(rename = "totalDataBytes", skip_serializing_if = "Option::is_none")]
-    pub total_data_bytes: Option<i64>,
-    #[serde(rename = "totalRepoBytes", skip_serializing_if = "Option::is_none")]
-    pub total_repo_bytes: Option<i64>,
-    #[serde(rename = "allocatedDataBytes", skip_serializing_if = "Option::is_none")]
-    pub allocated_data_bytes: Option<i64>,
-    #[serde(rename = "allocatedRepoBytes", skip_serializing_if = "Option::is_none")]
-    pub allocated_repo_bytes: Option<i64>,
-    #[serde(rename = "totalJobs", skip_serializing_if = "Option::is_none")]
-    pub total_jobs: Option<i64>,
-    #[serde(rename = "runningJobs", skip_serializing_if = "Option::is_none")]
-    pub running_jobs: Option<i64>,
+    #[serde(rename = "totalUsersRegistered", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub total_users_registered: Option<Option<i64>>,
+    #[serde(rename = "totalDataPoints", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub total_data_points: Option<Option<i64>>,
+    #[serde(rename = "totalRepoPoints", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub total_repo_points: Option<Option<i64>>,
+    #[serde(rename = "maxDataBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub max_data_bytes: Option<Option<i64>>,
+    #[serde(rename = "maxRepoBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub max_repo_bytes: Option<Option<i64>>,
+    #[serde(rename = "allocatedDataBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub allocated_data_bytes: Option<Option<i64>>,
+    #[serde(rename = "allocatedRepoBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub allocated_repo_bytes: Option<Option<i64>>,
+    #[serde(rename = "totalJobs", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub total_jobs: Option<Option<i64>>,
+    #[serde(rename = "totalRunningJobs", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub total_running_jobs: Option<Option<i64>>,
+    #[serde(rename = "freeDataBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub free_data_bytes: Option<Option<i64>>,
+    #[serde(rename = "freeRepoBytes", default, with = "::serde_with::rust::double_option", skip_serializing_if = "Option::is_none")]
+    pub free_repo_bytes: Option<Option<i64>>,
+    #[serde(rename = "averageCpu", skip_serializing_if = "Option::is_none")]
+    pub average_cpu: Option<Box<models::CpuItemDto>>,
 }
 
 impl SystemStatusDto {
     pub fn new() -> SystemStatusDto {
         SystemStatusDto {
-            cp_us: None,
-            average_cpu: None,
+            cpus: None,
+            errors: None,
             api_uptime: None,
             system_uptime: None,
             api_version: None,
@@ -57,12 +63,15 @@ impl SystemStatusDto {
             total_users_registered: None,
             total_data_points: None,
             total_repo_points: None,
-            total_data_bytes: None,
-            total_repo_bytes: None,
+            max_data_bytes: None,
+            max_repo_bytes: None,
             allocated_data_bytes: None,
             allocated_repo_bytes: None,
             total_jobs: None,
-            running_jobs: None,
+            total_running_jobs: None,
+            free_data_bytes: None,
+            free_repo_bytes: None,
+            average_cpu: None,
         }
     }
 }
